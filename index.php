938b29a56f71a65437e951d08f48f096c5e588ec
FANNYMU <154881121+FANNYMU@users.noreply.github.com>
2024-05-06 22:31:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Briem+Hand:wght@100..900&family=Jaro:opsz@6..72&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Silkscreen&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style/styled.css">
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <link rel="icon" href="/images/logo.svg">
+    <title>Home</title>
+</head>
+
+<body>
+    <nav>
+        <a href="">
+            <h1>Film</h1>
+        </a>
+    </nav>
+    <div class="container">
+
+        <div class="card">
+        <h1>Hot!</h1>
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card5">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card5">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card2">
+            <h1>Popular</h1>
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card3">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card3">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card2">
+            <h1>New</h1>
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card3">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card3">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card1">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+    <div class="container">
+        <div class="card6">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card6">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+        <div class="card6">
+            <img src="/images/fast.jpg" alt="img">
+        </div>
+    </div>
+</body>
+
+</html>
